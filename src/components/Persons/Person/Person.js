6a21e4a906858b6f205307c591f9aad9a248d2ef
pawel2973 <pawel2973@gmail.com>
2019-04-09 19:39:07 +0200

    Using PropTypes to prevent receive incorrect data to props
    - Install externall community library
    npm install --save prop-types
    - Declarate data types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import styles from './Person.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import withClasses from '../../../hoc/withClass';
@@ -9,15 +10,22 @@ class Person extends Component {
         return (
             <Aux>
                 {/*<div className={styles.Person}>*/}
-                    <h1>{this.props.name}</h1>
-                    <p>Age: {this.props.age}</p>
-                    <p>{this.props.children}</p>
-                    <input type="text" onChange={this.props.changed} value={this.props.name}/>
-                    <button onClick={this.props.click}>Delete me!</button>
+                <h1>{this.props.name}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <button onClick={this.props.click}>Delete me!</button>
                 {/*</div>*/}
             </Aux>
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClasses(Person, styles.Person); //export person function ES6 feature
